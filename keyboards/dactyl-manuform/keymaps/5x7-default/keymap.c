8b190b702a6d006eb6cca225f2f9b5f3240ee919
Akio Shigekane <motorado519+git@gmail.com>
2018-03-06 23:45:48 +0900

    スペースとバックスペースを入れ替え，デバイスID・プロダクトIDを変更

--- keyboards/dactyl-manuform/keymaps/5x7-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x7-default/keymap.c
@@ -52,18 +52,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,  KC_LSHIFT,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
     KC_NO,  KC_LCTRL,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,
                                     KC_ESC, KC_LGUI,
-                                                    KC_DEL,     KC_LSFT,
+                                                    KC_SPC,     KC_LSFT,
                                                     LT(_FN, KC_ENTER), KC_LCTRL,
-                                                    KC_SPC,     KC_LALT,
+                                                    KC_DEL,     KC_LALT,
         // right hand
     KC_6,        KC_7,      KC_8,   KC_9,   KC_0,   KC_MINS,    JP_HAT,
     KC_Y,        KC_U,      KC_I,   KC_O,   KC_P,   JP_AT,      JP_LBRACKET,
     KC_H,        KC_J,      KC_K,   KC_L,   KC_SCLN,KC_QUOT,    JP_RBRACKET,
     KC_N,        KC_M,      KC_COMM,KC_DOT, KC_SLSH,KC_RO,      KC_JYEN,
                             KC_PGUP, KC_PGDN,
-    KC_LSHIFT,         KC_BSPC,
+    KC_LSHIFT,         KC_SPC,
     KC_RCTRL,       LT(_FN, KC_ENTER),
-    KC_LALT,        KC_SPC),
+    KC_LALT,        KC_BSPC),
 [_FN] = KEYMAP_5x7(
   // left hand
    KC_NO,   RESET,      KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,
@@ -71,18 +71,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_NO,   KC_LSHIFT,  _______,SHIGE,  KC_LEFT,  KC_DOWN, KC_RIGHT,
    KC_NO,   KC_LCTRL,   _______,_______,  _______,  _______,  _______,
                         _______,_______,
-                                                    KC_DEL,     KC_LSFT,
+                                                    KC_SPC,     KC_LSFT,
                                                     LT(_FN, KC_ENTER), KC_LCTRL,
-                                                    KC_SPC,     KC_LALT,
+                                                    KC_DEL,     KC_LALT,
         // right hand
     KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,  KC_F11,  KC_F12,
     YOROSIKU,  KC_HOME,  KC_UP,    OTUKARE,   _______,  _______,  _______,
     KC_LEFT,  KC_DOWN,  KC_UP,  KC_RIGHT, _______, _______,  _______,
     KC_CALC,  KC_MAIL,  KC_MYCM,  KC_WHOM,  _______,   _______,  RESET,
                         _______,  _______,
-    KC_LSHIFT,         KC_BSPC,
+    KC_LSHIFT,         KC_SPC,
     KC_RCTRL,       LT(_FN, KC_ENTER),
-    KC_LALT,        KC_SPC),
+    KC_LALT,        KC_BSPC),
 };
 
 
